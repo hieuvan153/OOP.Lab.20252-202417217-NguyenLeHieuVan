cec55345ddb5ca22e8392ef9d069f5eaa823ae1a
hieuvan153 <hieuvan153282278@gmail.com>
2026-03-26 14:36:44 +0700

    Completed Input From Keyboard

--- Lab01/Ex61.java
+++ Lab01/Ex61.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class ChoosingOption {
+public class Ex61 {
     public static void main(String[] args) {
         int option = JOptionPane.showConfirmDialog(null, "Do you want to change the first class ticket");
         JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION ? "YES" : "NO"));
