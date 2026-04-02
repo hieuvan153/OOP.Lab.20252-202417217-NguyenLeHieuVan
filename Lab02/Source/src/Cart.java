1593afeba8cc7cb7554cd0ba574d1d58632501e7
hieuvan153 <hieuvan153282278@gmail.com>
2026-04-02 14:43:38 +0700

    Fixed Use Case Diagram and Class Diagram

---
+++ Lab02/Source/src/Cart.java
@@ -0,0 +1,17 @@
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+			
+			if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+				
+			}
+		}
+	}
+}
